9dc1579403a83bab24778bf0cbda7ae37a7d9857
Grainger <39377884+remixor@users.noreply.github.com>
2021-03-25 21:33:29 +0100

    Add files via upload

---
+++ commands/ping.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (client, message) => {
+    message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+}
+
+module.exports.help = {
+	name: 'ping',
+	description: 'Checks API Latency',
+	execute(message, args) {
+        message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+        if (!client.commands.has(command)) return;
+
+try {
+	client.commands.get(command).execute(bot, message);
+} catch (error) {
+	console.error(error);
+	message.reply('there was an error trying to execute that command!');
+}
+	},
+};
